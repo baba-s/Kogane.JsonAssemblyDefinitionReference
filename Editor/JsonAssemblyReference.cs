22dbb09fa58b19139ee7192a23a803658d436a65
baba-s <shotababa1206@gmail.com>
2022-09-24 19:14:27 +0900

    add

---
+++ Editor/JsonAssemblyReference.cs
@@ -0,0 +1,13 @@
+using System;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable NotAccessedField.Global
+
+namespace Kogane
+{
+    [Serializable]
+    public sealed class JsonAssemblyReference
+    {
+        public string reference = string.Empty;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.JsonAssemblyReference
+# Kogane Json Assembly Reference
+
+Assembly Reference（.asmref）の JSON を表すクラス
